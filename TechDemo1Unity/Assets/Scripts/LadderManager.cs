699e2774165a7ee31b5ae86b66add4c9b33a265e
DM <dommcneill@outlook.com>
2024-10-15 02:49:29 +0100

    started work on ladders

---
+++ TechDemo1Unity/Assets/Scripts/LadderManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderManager : MonoBehaviour
+{
+	public bool IsPlayerOnLadder = false;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
