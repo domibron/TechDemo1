a5a3f2d7137363808cd40d41c453553d8f707298
DM <dommcneill@outlook.com>
2024-10-17 16:03:30 +0100

    work

---
+++ TechDemo1Unity/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject AreYouSurePopUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClosePopUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+    public void QuitButtonPressed()
+    {
+        AreYouSurePopUp.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ClosePopUp()
+    {
+        AreYouSurePopUp.SetActive(false);
+    }
+
+
+}
